c5623b6b196dc6fa31593510d4c8d52c47088876
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-10 12:08:43 -0700

    initial setup

---
+++ pigLatin/pigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  pigLatin
+//
+//  Created by Nathan Wainwright on 2018-08-10.
+//  Copyright © 2018 Nathan Wainwright. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
