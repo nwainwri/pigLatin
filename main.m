2347ae82bdefae740577485693ed644c076a9108
Nathan Wainwright <nathanwainwright@gmail.com>
2018-08-10 23:25:37 -0700

    added "lowercase" to userInput string; easier.

--- main.m
+++ main.m
@@ -13,31 +13,35 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        //CONSONTANT ARRAYS
+        NSArray *twoLetter = @[@"sm",@"sn",@"st",@"sw",@"sk",@"sl",@"sp",@"dw",@"tw",@"th",@"sp",@"dr",@"tr",@"qu",@"cr",@"cl",@"pr",@"fr",@"br",@"gr",@"pl",@"fl",@"bl",@"gl"];
+        NSArray *threeLetter = @[@"shr",@"thr",@"thw",@"sph",@"spl",@"spr",@"str",@"scr",@"squ"];
+        
         NSMutableString *finalString = [@"" mutableCopy];
         NSMutableString *userInput = [@"That'll" mutableCopy];
-        NSMutableString *searchString = [@"Th" mutableCopy];
+        NSMutableString *searchString = [@"th" mutableCopy];
         NSMutableString *userInputSentence = [@"This'll be our f'orst T'aste" mutableCopy];
         
-//        PigLatin *pigword = [[PigLatin alloc] init];
-//        //WORD TEST
-//        userInput = [[pigword makePigLatin:userInput] mutableCopy];
-//        NSLog(@"WORD TEST: %@", userInput);
-//        //SENTANCE TEST
-//        finalString = [[pigword makePigLatinSentence:userInputSentence] mutableCopy];
-//        NSLog(@"SENTENCE TEST: %@", finalString);
-//        // TRICK TO REMOVE ALL SPACES FROM A SENTENCE BEOFRE TOSSING INTO ARRAY
-//        // https://stackoverflow.com/questions/4814641/how-to-separate-string-by-space-using-objective-c
-//
-//        //WORD TEST WITH CATEGORY --  WORKS
-//        [userInput wordByPigLatinization];
-//        NSLog(@"CATEGORY TEST; WORD: %@", userInput);
-//
-//        //SENTENCE TEST WITH CATEGORY
-//        userInputSentence = [[userInputSentence stringByPigLatinization] mutableCopy];
-//        NSLog(@"CATEGORY TEST: SENTENCT: %@", userInputSentence);
+        //        PigLatin *pigword = [[PigLatin alloc] init];
+        //        //WORD TEST
+        //        userInput = [[pigword makePigLatin:userInput] mutableCopy];
+        //        NSLog(@"WORD TEST: %@", userInput);
+        //        //SENTANCE TEST
+        //        finalString = [[pigword makePigLatinSentence:userInputSentence] mutableCopy];
+        //        NSLog(@"SENTENCE TEST: %@", finalString);
+        //        // TRICK TO REMOVE ALL SPACES FROM A SENTENCE BEOFRE TOSSING INTO ARRAY
+        //        // https://stackoverflow.com/questions/4814641/how-to-separate-string-by-space-using-objective-c
+        //
+        //        //WORD TEST WITH CATEGORY --  WORKS
+        //        [userInput wordByPigLatinization];
+        //        NSLog(@"CATEGORY TEST; WORD: %@", userInput);
+        //
+        //        //SENTENCE TEST WITH CATEGORY
+        //        userInputSentence = [[userInputSentence stringByPigLatinization] mutableCopy];
+        //        NSLog(@"CATEGORY TEST: SENTENCT: %@", userInputSentence);
         
-//        NEXT STEP IS MAKING A CONSONANT DICTIONARY AND USING TO CHECK WORDS FOR themeBadTextColorErr
-//        https://www.sltinfo.com/syllables-and-clusters/
+        //        NEXT STEP IS MAKING A CONSONANT DICTIONARY AND USING TO CHECK WORDS FOR themeBadTextColorErr
+        //        https://www.sltinfo.com/syllables-and-clusters/
         
         // first search a string for just "Th"
         // return that as a test.
@@ -49,39 +53,43 @@ int main(int argc, const char * argv[]) {
         
         // use above code to modify 'string return piglat'
         
-//        NSString *string = @"lorem ipsum dolor sit amet";
-//        NSLog(@"TEST: %@", string);
-//        NSRange range = [string rangeOfString:@"ipsum"];
-//        // {.location=6, .length=5}
-//        NSString *substring = [string substringWithRange:range];
-//        NSLog(@"TEST: %@", substring);
-//        // @"ipsum"
+        //        NSString *string = @"lorem ipsum dolor sit amet";
+        //        NSLog(@"TEST: %@", string);
+        //        NSRange range = [string rangeOfString:@"ipsum"];
+        //        // {.location=6, .length=5}
+        //        NSString *substring = [string substringWithRange:range];
+        //        NSLog(@"TEST: %@", substring);
+        //        // @"ipsum"
         
-//        NSRange rangeTest = [userInput rangeOfString:searchString];
-//
-//
-//        NSMutableString *substring2 = [[userInput substringWithRange:rangeTest] mutableCopy];
-//
-//        NSLog(@"TEST: %@", substring2);
+        //        NSRange rangeTest = [userInput rangeOfString:searchString];
+        //
+        //
+        //        NSMutableString *substring2 = [[userInput substringWithRange:rangeTest] mutableCopy];
+        //
+        //        NSLog(@"TEST: %@", substring2);
+        
+        userInput = [[userInput lowercaseString] mutableCopy]; // takes userInput and drops it to lowercase
+        
         NSRange rangeTest = [userInput rangeOfString:searchString];
-//        NSRange range = NSMakeRange(0, 1);
+        //        NSRange range = NSMakeRange(0, 1);
         NSMutableString *firstPart = [@"" mutableCopy];
         NSLog(@"firstPart: %@", firstPart);
         userInput = [[userInput componentsSeparatedByCharactersInSet:[[NSCharacterSet letterCharacterSet] invertedSet]] componentsJoinedByString:@""];
-        NSLog(@"userInput PUNC REMOVED: %@", userInput);
+        //NSLog(@"userInput PUNC REMOVED: %@", userInput);
+        // this finds "Th" (consonant" and removes from front... puts on end... adds "ay" ...
         
         
         
-        // this finds "Th" (consonant" and removes from front... puts on end... adds "ay" ... 
         firstPart = [[searchString substringWithRange:rangeTest] mutableCopy];
-        NSLog(@"substringWithRange: firstPart %@", firstPart);
+        //NSLog(@"substringWithRange: firstPart %@", firstPart);
         
         userInput = [[userInput stringByReplacingCharactersInRange:rangeTest withString:@""] mutableCopy];
-        NSLog(@"stringByReplacingCharactersInRange: %@", userInput);
+        //NSLog(@"stringByReplacingCharactersInRange: %@", userInput);
         userInput = [[userInput stringByAppendingString:firstPart] mutableCopy];
-        NSLog(@"stringByAppendingString: %@", userInput);
+        //NSLog(@"stringByAppendingString: %@", userInput);
         firstPart = [[userInput stringByAppendingString:@"ay"] mutableCopy];
-        NSLog(@"stringByAppendingString: %@", firstPart);
+        //NSLog(@"stringByAppendingString: %@", firstPart);
+        
 
     }
     return 0;
